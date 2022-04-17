bc04ccd614500c3cbb1f70d0c9363209db0d94d5
KazumaTani <ktani@g.ecc.u-tokyo.ac.jp>
2022-04-17 21:01:07 +0900

    Create the programming langage which can calculate four arithmetic operations

--- 9cc.c
+++ 9cc.c
@@ -24,6 +24,41 @@ struct Token {
 
 Token *token;
 
+typedef enum {
+	ND_ADD,
+	ND_SUB,
+	ND_MUL,
+	ND_DIV,
+	ND_NUM
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node{
+	NodeKind kind;
+	Node *lhs;
+	Node *rhs;
+	int val;
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs){
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+
+Node *new_node_num(int val){
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = ND_NUM;
+	node->val = val;
+	return node;
+}
+
+
+
 char *user_input;
 
 void error(char *fmt, ...) {
@@ -40,7 +75,7 @@ void error_at(char *loc, char *fmt, ...){
 
 	int pos = loc - user_input;
 	fprintf(stderr, "%s\n", user_input);
-	fprintf(stderr, "%s*s", pos, " ");
+	fprintf(stderr, "%*s", pos, " ");
 	fprintf(stderr, "^ ");
 	vfprintf(stderr, fmt, ap);
 	fprintf(stderr, "\n");
@@ -53,6 +88,9 @@ bool consume(char op){
 	token = token->next;
 	return true;
 }
+
+
+
 
 void expect(char op) {
 	if (token->kind != TK_RESERVED || token->str[0] != op)
@@ -92,7 +130,7 @@ Token *tokenize() {
 			continue;
 		}
 
-		if (*p == '+' || *p == '-') {
+		if (strchr("+-*/()", *p)) {
 			cur = new_token(TK_RESERVED, cur, p++);
 			continue;
 		}
@@ -111,6 +149,83 @@ Token *tokenize() {
 }
 
 
+Node *expr();
+Node *mul();
+Node *unary();
+Node *primary();
+
+Node *expr(){
+	Node *node = mul();
+
+	for (;;){
+		if (consume('+'))
+			node = new_node(ND_ADD, node, mul());
+		else if (consume('-'))
+			node = new_node(ND_SUB, node, mul());
+		else
+			return node;
+	}
+}
+
+Node *mul(){
+	Node *node = unary();
+
+	for (;;) {
+		if (consume('*'))
+			node = new_node(ND_MUL, node, unary());
+		else if (consume('/'))
+			node = new_node(ND_DIV, node, unary());
+		else
+			return node;
+	}
+}
+
+Node *unary(){
+	if (consume('+'))
+		return primary();
+	if (consume('-'))
+		return new_node(ND_SUB, new_node_num(0), primary());
+	return primary();
+}
+
+Node *primary(){
+	if (consume('(')){
+		Node *node = expr();
+		expect(')');
+		return node;
+	}
+	return new_node_num(expect_number());
+}
+
+void gen(Node *node){
+	if (node->kind == ND_NUM){
+		printf("  push %d\n", node->val);
+		return;
+	}
+
+	gen(node->lhs);
+	gen(node->rhs);
+
+	printf("  pop rdi\n");
+	printf("  pop rax\n");
+
+	switch (node->kind){
+		case ND_ADD:
+			printf("  add rax, rdi\n");
+			break;
+		case ND_SUB:
+			printf("  sub rax, rdi\n");
+			break;
+		case ND_MUL:
+			printf("  imul rax, rdi\n");
+			break;
+		case ND_DIV:
+			printf("  cqo\n");
+			printf("  idiv rdi\n");
+			break;
+	}
+	printf("  push rax\n");
+}
 
 int main(int argc, char **argv) {
 	if (argc != 2) {
@@ -118,26 +233,16 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 	user_input = argv[1];
-	token = tokenize();
+	token = tokenize(user_input);
+	Node *node = expr();
 
 
 	printf(".intel_syntax noprefix\n");
 	printf(".globl main\n");
 	printf("main:\n");
-	printf("  mov rax, %d\n", expect_number());
-	
-	while (!at_eof()) {
-		if (consume('+')) {
-			printf("  add rax, %d\n", expect_number());
-			continue;
-		}
-		expect('-');
-		printf("  sub rax, %d\n", expect_number());
 
-
-	}
-
-
+	gen(node);
+	printf("  pop rax\n");
   	printf("  ret\n");
 	return 0;
 }
--- Readme.md
+++ Readme.md
@@ -1,3 +1,19 @@
-# Reference
+# Cコンパイラ
+
+## Reference
 
 [低レイヤを知りたい人のためのCコンパイラ作成入門](https://www.sigbus.info/compilerbook)
+
+## EBNF
+
+* a* = aの0回以上の繰り返し
+* a? = Aまたはε
+* (...) = グループ化 
+
+## 再帰下降構文解析
+
+```
+expr = mul ("+" mul | "-" mul)*
+mul  = primary ("*" primary | "/" primary)*
+primary = num | "(" expr ")"
+```
